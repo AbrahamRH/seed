171f9f59bdf2f41e995ff4cb4009609463565182
AbrahamRH <abrahamrzhz@gmail.com>
2024-06-05 22:44:23 -0600

    feat: agregando politica de retencion de 3 dias para la FRA

--- s-07-habilita-fra.sql
+++ s-07-habilita-fra.sql
@@ -6,7 +6,7 @@ alter system set db_recovery_file_dest_size=20g scope=spfile;
 
 alter system set db_recovery_file_dest='/unam-diplomado-bd/seed/d06/app/oracle/oradata/SEED/FRA' scope=spfile;
 
-alter system set db_flashback_retention_target=720 scope=spfile;
+alter system set db_flashback_retention_target=4320 scope=spfile;
 
 shutdown immediate
 startup
