a68641717545a52ef718eb049869f3c025ccb043
AbrahamRH <abrahamrzhz@gmail.com>
2024-06-13 00:11:00 -0600

    backups

---
+++ rman-backups.sh
@@ -0,0 +1,4 @@
+backup as backupset database tag bk_set format '/unam-diplomado-bd/seed/d11/app/oracle/oradata/SEED/%u_%p_%c';
+backup as backupset database tag bk_set_fra;
+backup as copy device type disk database tag bk_copy;
+backup as copy device type disk database tag bk_copy fotmat '/unam-diplomado-bd/seed/d11/app/oracle/oradata/SEED/data-D-%d-id-%I_TS-%N_FNO-%f_%u'
--- s-util-consultas.sql
+++ s-util-consultas.sql
@@ -1,5 +1,26 @@
+set linesize window
+
 -- mostrar usuarios de las pdbs
-select u.username, p.con_id, p.pdb_name, u.created from cdb_users u, dba_pdbs p where p.pdb_id=u.con_id and username like 'ADMIN%';
+col pdb_name format a20
+col username format a20
+select u.username, p.con_id, p.pdb_name, u.created 
+    from cdb_users u, dba_pdbs p 
+    where p.pdb_id=u.con_id 
+    and username like 'ADMIN%';
+
+
+-- Consulta de la ubicacion de los datafiles
+col pdb_name format a20
+col tablespace_name format a20
+col file_name format a80
+select p.pdb_id, p.pdb_name, d.file_id, d.tablespace_name, d.file_name
+    from dba_pdbs p, cdb_data_files d
+    where p.pdb_id = d.con_id
+    order by p.pdb_id;
+
+-- Consulta de la informacion para los redologs
+col member format a80
+select group#, type, member from v$logfile;
 
 -- Uso de la FRA
 col name format a20
