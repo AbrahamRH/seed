341f689cc3ee54a81513a008a1f547bad9aaeba0
gustavovega55 <gustavovega55@aragon.unam.mx>
2024-06-14 21:59:09 -0600

    Add files via upload

---
+++ carga/ESTATUS_VIAJE.sql
@@ -0,0 +1,6 @@
+INSERT INTO ESTATUS_VIAJE (estatus_id, estatus_viaje, descripcion) VALUES (1, 'cargando', 'El viaje está en proceso de carga');
+INSERT INTO ESTATUS_VIAJE (estatus_id, estatus_viaje, descripcion) VALUES (2, 'en curso', 'El viaje está en curso hacia su destino');
+INSERT INTO ESTATUS_VIAJE (estatus_id, estatus_viaje, descripcion) VALUES (3, 'en centro de acopio', 'El viaje ha llegado al centro de acopio');
+INSERT INTO ESTATUS_VIAJE (estatus_id, estatus_viaje, descripcion) VALUES (4, 'descargando', 'El viaje está en proceso de descarga');
+INSERT INTO ESTATUS_VIAJE (estatus_id, estatus_viaje, descripcion) VALUES (5, 'en destino del cliente', 'El viaje ha llegado al destino del cliente');
+INSERT INTO ESTATUS_VIAJE (estatus_id, estatus_viaje, descripcion) VALUES (6, 'en pausa', 'El viaje está en pausa');
---
+++ carga/Reg_CENTRO_ACOPIO.sql
@@ -0,0 +1,8 @@
+INSERT INTO centros_acopio (centro_acopio_id, nombre_centro, direccion_centro, latitud_centro, longitud_centro, empleado_id)
+VALUES 
+(1, 'Centro A', 'Calle José González 563, Benito Juárez, 83110 Hermosillo, Son.', 29.12345, -110.98765, 21),
+(2, 'Centro B', 'Pitiquito, Sonora.', 30.23456, -111.87654, 22),
+(3, 'Centro C', 'Puerto Peñasco, Sonora.', 31.34567, -112.76543, 23),
+(4, 'Centro D', 'Dirección de Centro D', 32.45678, -113.65432, 24),
+(5, 'Centro E', 'HF94+5P3 Jaraguay, Baja California', 29.56789, -114.54321, 25),
+(6, 'Centro F', 'San Pedro, Coah.', 26.386310, -102.673854, 26);
