be0cb67994216ff84d2404197e521c4c2abf35bd
AbrahamRH <abrahamrzhz@gmail.com>
2024-06-14 20:22:10 -0600

    grants to cdb_admin

--- s-10-permisos-usuarios.sql
+++ s-10-permisos-usuarios.sql
@@ -3,7 +3,7 @@ connect sys/system2@seed as sysdba
 Prompt creando usuario admin root
 
 create user c##cdb_admin identified by cdb_admin container=all;
-
+grant create session, create table, create procedure, create sequence to admin_viajes;
 
 prompt otorgando permisos a usuario viajes
 connect sys/system2@pdb_viajes as sysdba
